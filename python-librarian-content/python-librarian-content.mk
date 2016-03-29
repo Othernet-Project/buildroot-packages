63d4fac2a6987378698de86b5172f986f159b7ef
Branko Vukelic <branko@outernet.is>
2016-03-29 18:24:24 +0200

    Updated librarian-content and librarian-filemanager
    Fixes bug related to thumbnail generation
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-librarian-content/python-librarian-content.mk
+++ python-librarian-content/python-librarian-content.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_CONTENT_VERSION = fa0bfeeb79b4ddc5a4400e76cee546ed04fb4b08
+PYTHON_LIBRARIAN_CONTENT_VERSION = 70945117617af7f6651512752407cba09137fe45
 PYTHON_LIBRARIAN_CONTENT_SITE = $(call github,Outernet-Project,librarian-content,$(PYTHON_LIBRARIAN_CONTENT_VERSION))
 PYTHON_LIBRARIAN_CONTENT_LICENSE = GPL
 PYTHON_LIBRARIAN_CONTENT_SETUP_TYPE = setuptools
