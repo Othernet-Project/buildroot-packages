b3c976c4c079397b1110748173cbe4e1ec57e6eb
Branko Vukelic <branko@outernet.is>
2016-04-01 20:31:46 +0200

    Updated librarian-content and librarian-filemanager
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-librarian-content/python-librarian-content.mk
+++ python-librarian-content/python-librarian-content.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_CONTENT_VERSION = 4abd919329601ad1cc5c6cb3703de00465ec79ac
+PYTHON_LIBRARIAN_CONTENT_VERSION = f94cfb358d05abb18db4c3481551322f107eb012
 PYTHON_LIBRARIAN_CONTENT_SITE = $(call github,Outernet-Project,librarian-content,$(PYTHON_LIBRARIAN_CONTENT_VERSION))
 PYTHON_LIBRARIAN_CONTENT_LICENSE = GPL
 PYTHON_LIBRARIAN_CONTENT_SETUP_TYPE = setuptools
