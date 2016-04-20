1f9fcc7e0e939f5a6f5ac5ce572d779bb1ea71ea
Branko Vukelic <branko@outernet.is>
2016-04-20 19:15:33 +0200

    Do not make 'v' part of the site
    This allows hashes to be used in addition to tags.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-bitpack/python-bitpack.mk
+++ python-bitpack/python-bitpack.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_BITPACK_VERSION = 0.2
-PYTHON_BITPACK_SITE = $(call github,Outernet-Project,bitpack,v$(PYTHON_BITPACK_VERSION))
+PYTHON_BITPACK_VERSION = v0.2
+PYTHON_BITPACK_SITE = $(call github,Outernet-Project,bitpack,$(PYTHON_BITPACK_VERSION))
 PYTHON_BITPACK_LICENSE = BSD
 PYTHON_BITPACK_LICENSE_FILES = LICENSE
 PYTHON_BITPACK_SETUP_TYPE = setuptools
