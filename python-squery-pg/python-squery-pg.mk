953b48fff36b9694f7bb5cb2b605206eaa099381
Branko Vukelic <branko@outernet.is>
2016-04-21 12:11:54 +0200

    Updated squery-pg
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-squery-pg/python-squery-pg.mk
+++ python-squery-pg/python-squery-pg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_SQUERY_PG_VERSION = v1.2
+PYTHON_SQUERY_PG_VERSION = v1.3
 PYTHON_SQUERY_PG_SITE = $(call github,Outernet-Project,squery-pg,$(PYTHON_SQUERY_PG_VERSION))
 PYTHON_SQUERY_PG_LICENSE = GPL
 PYTHON_SQUERY_PG_SETUP_TYPE = setuptools
