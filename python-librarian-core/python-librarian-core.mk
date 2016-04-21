ea181a4d61be61acc136fdc346b0cef5f3ce7590
Branko Vukelic <branko@outernet.is>
2016-04-21 17:14:11 +0200

    Updated librarian-filemanager and librarian-core
    Fixes issues related to translations not appearing
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-librarian-core/python-librarian-core.mk
+++ python-librarian-core/python-librarian-core.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_CORE_VERSION = ee1d68319a27585fa1dbd5e3107aef3ac2684c5e
+PYTHON_LIBRARIAN_CORE_VERSION = v1.2
 PYTHON_LIBRARIAN_CORE_SITE = $(call github,Outernet-Project,librarian-core,$(PYTHON_LIBRARIAN_CORE_VERSION))
 PYTHON_LIBRARIAN_CORE_LICENSE = GPL
 PYTHON_LIBRARIAN_CORE_SETUP_TYPE = setuptools
