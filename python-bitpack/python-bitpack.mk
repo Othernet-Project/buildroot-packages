033c58cc999c37546b0b6e26f2ff16acc4bfa354
Branko Vukelic <branko@outernet.is>
2016-04-22 11:43:15 +0200

    Updated bitpack
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-bitpack/python-bitpack.mk
+++ python-bitpack/python-bitpack.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BITPACK_VERSION = v0.1
+PYTHON_BITPACK_VERSION = v1.0
 PYTHON_BITPACK_SITE = $(call github,Outernet-Project,bitpack,$(PYTHON_BITPACK_VERSION))
 PYTHON_BITPACK_LICENSE = BSD
 PYTHON_BITPACK_LICENSE_FILES = LICENSE
