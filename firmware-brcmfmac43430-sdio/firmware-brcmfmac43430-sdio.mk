c574cc5c1258ac1846e9872486c516102f9fd637
Branko Vukelic <branko@outernet.is>
2016-05-04 20:27:00 +0200

    [firmware-brcmfmac43430-sdio] Make doc dir
    The build would fail if doc dir was not present at /usr/share/doc, so the
    package makefile should create it.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- firmware-brcmfmac43430-sdio/firmware-brcmfmac43430-sdio.mk
+++ firmware-brcmfmac43430-sdio/firmware-brcmfmac43430-sdio.mk
@@ -11,6 +11,7 @@ FIRMWARE_BRCMFMAC43430_SDIO_LICENSE_FILE = brcm80211/LICENSE
 
 define FIRMWARE_BRCMFMAC43430_SDIO_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware/brcm
+	mkdir -p $(TARGET_DIR)/usr/share/doc
 	install -m644 $(@D)/brcm80211/brcm/brcmfmac43430-sdio.bin \
 		$(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.bin
 	install -m644 $(@D)/brcm80211/brcm/brcmfmac43430-sdio.txt \
