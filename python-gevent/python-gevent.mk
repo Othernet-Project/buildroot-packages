6991faf2f3cc9c54da12e6e35d774f314d23c391
Branko Vukelic <branko@outernet.is>
2016-05-17 23:21:51 +0200

    Updated gevent to 1.1.1
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-gevent/python-gevent.mk
+++ python-gevent/python-gevent.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_GEVENT_VERSION = 1.0.1
+PYTHON_GEVENT_VERSION = 1.1.1
 PYTHON_GEVENT_SOURCE = gevent-$(PYTHON_GEVENT_VERSION).tar.gz
 PYTHON_GEVENT_SITE = https://pypi.python.org/packages/source/g/gevent
 PYTHON_GEVENT_LICENSE = MIT
