d36403df17d432a92ec70ca35262a1d451e139f4
Branko Vukelic <branko@outernet.is>
2016-05-18 23:02:00 +0200

    Added missing init scripts to packages
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- ondd/ondd.mk
+++ ondd/ondd.mk
@@ -5,15 +5,23 @@
 ################################################################################
 
 ONDD_VERSION = 2.1.0
-ONDD_SOURCE = ondd-$(ONDD_VERSION).tar.gz
-ONDD_SITE = https://outernet.is
-ONDD_DEPENDENCIES = openssl
+ONDD_SITE = $(BR2_EXTERNAL)/package/ondd/src
+ONDD_SITE_METHOD = local
 ONDD_LICENSE = PROPRIETARY
+
+ifeq ($(BR2_PACKAGE_ONDD_BUILD),y)
+ONDD_DEPENDENCIES = openssl
 
 define ONDD_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D) INSTALL_PREFIX=$(TARGET_DIR) \
 		CFLAGS=-fpermissive \
 		CC=$(TARGET_CC) clean debug release install
+endef
+endif
+
+define ONDD_INSTALL_INIT_SYSV
+	$(INSTALL) -Dm0755 $(BR2_EXTERNAL)/package/ondd/S90ondd \
+		$(TARGET_DIR)/etc/init.d/S90ondd
 endef
 
 $(eval $(generic-package))
