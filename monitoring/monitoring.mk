fbfa1d18cd8f985e4528f9f3ad58659e0a2d15f2
Branko Vukelic <branko@outernet.is>
2016-05-21 01:01:53 +0200

    Made persist dir and librarian dir configurable
    The persist dir could be (and, indeed is) different from platform to platform,
    so we made that configurable via kconfig. The librarian settings path is now
    set using the $BR2_LIBRARIAN_SETTINGS_FILE variable. It is up to the build to
    assign a value to this variable, though, and no menu items is provided.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- monitoring/monitoring.mk
+++ monitoring/monitoring.mk
@@ -10,10 +10,13 @@ MONITORING_LICENSE = GPLv3+
 MONITORING_LICENSE_FILES = COPYING
 MONITORING_SETUP_TYPE = setuptools
 
+MONITORING_SED_CMDS += s|%PERSISTDIR%|$(call qstrip,$(BR2_MONITORING_PERSISTDIR))|;
+MONITORING_SED_CMDS += s|%LIBRARIAN_SETTINGS_FILE%|$(call qstrip,$(BR2_LIBRARIAN_SETTINGS_FILE))|;
+
 define MONITORING_INSTALL_INIT_SYSV
 	$(INSTALL) -Dm0755 $(BR2_EXTERNAL)/package/monitoring/S91monitoring \
 		$(TARGET_DIR)/etc/init.d/S91monitoring
+	sed -i '$(MONITORING_SED_CMDS)' $(TARGET_DIR)/etc/init.d/S91monitoring
 endef
-
 
 $(eval $(python-package))
