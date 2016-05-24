ec45e94ba7032d2f41a3436126df59b82b1448e1
Branko Vukelic <branko@outernet.is>
2016-05-24 20:00:58 +0200

    Added lftp package
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ python-lftp/python-lftp.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# python-lftp
+#
+################################################################################
+
+PYTHON_LFTP_VERSION = 7b03ef18d9c7073ea80149a057d3f573a48974f0
+PYTHON_LFTP_SITE = $(call github,Outernet-Project,lftp,$(PYTHON_LFTP_VERSION))
+PYTHON_LFTP_LICENSE = GPLv3
+PYTHON_LFTP_LICENSE_FILES = COPYING
+PYTHON_LFTP_SETUP_TYPE = setuptools
+
+ifeq ($(BR2_PACKAGE_PYTHON_LFTP),y)
+LIBRARIAN_COMPONENTS += lftp
+endif
+
+# We always include these
+LIBRARIAN_SED_COMMANDS += s|%FTP_BASEDIRS%|$(shell $(BR2_EXTERNAL)/scripts/listify.sh $(call qstrip,$(BR2_LFTP_BASEDIRS)))|;
+
+$(eval $(python-package))
