c40dad4569252746d7f2b11acc21f0e9f7033262
Branko Vukelic <branko@outernet.is>
2016-06-01 20:59:10 +0200

    Updated lftp to v1.0
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-lftp/python-lftp.mk
+++ python-lftp/python-lftp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LFTP_VERSION = 7b03ef18d9c7073ea80149a057d3f573a48974f0
+PYTHON_LFTP_VERSION = v1.0
 PYTHON_LFTP_SITE = $(call github,Outernet-Project,lftp,$(PYTHON_LFTP_VERSION))
 PYTHON_LFTP_LICENSE = GPLv3
 PYTHON_LFTP_LICENSE_FILES = COPYING
