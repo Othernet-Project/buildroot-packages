071f1eb62896453261f8f85e0486f0cc79652f89
Branko Vukelic <branko@outernet.is>
2016-06-02 16:24:04 +0200

    Updated FSAL to v2.0
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-fsal/python-fsal.mk
+++ python-fsal/python-fsal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_FSAL_VERSION = ef6237f8a3def290822394c5f96306030ab96048
+PYTHON_FSAL_VERSION = v2.0
 PYTHON_FSAL_SITE = $(call github,Outernet-Project,fsal,$(PYTHON_FSAL_VERSION))
 PYTHON_FSAL_LICENSE = GPLv3+
 PYTHON_FSAL_LICENSE_FILES = COPYING
