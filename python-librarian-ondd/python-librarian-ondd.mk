ede613ea03472911c1a982bbd68b5278fad69f49
Branko Vukelic <branko@outernet.is>
2016-06-02 16:24:54 +0200

    Updated librarian-ondd to v1.2
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-librarian-ondd/python-librarian-ondd.mk
+++ python-librarian-ondd/python-librarian-ondd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ONDD_VERSION = 128ee79b5e1bdab488662502991efea7b5fae758
+PYTHON_LIBRARIAN_ONDD_VERSION = v1.2
 PYTHON_LIBRARIAN_ONDD_SITE = $(call github,Outernet-Project,librarian-ondd,$(PYTHON_LIBRARIAN_ONDD_VERSION))
 PYTHON_LIBRARIAN_ONDD_LICENSE = GPLv3+
 PYTHON_LIBRARIAN_ONDD_LICENSE_FILES = COPYING
