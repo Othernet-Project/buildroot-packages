86e8a12de534a4b03aada7e8a7f0e84ce3ae3d39
Branko Vukelic <branko@outernet.is>
2016-06-04 13:17:36 +0200

    Updated packages
    Brings listed packages to rxOS level.
    
    - fsal -> v2.0
    - librarian-filemanager -> v1.6
    - librarian-ondd -> v1.2
    - librarian-notifications -> v1.3
    - librarian-logs -> v1.1
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-librarian-logs/python-librarian-logs.mk
+++ python-librarian-logs/python-librarian-logs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_LOGS_VERSION = ffb996c33ef64e716a82442b9a6fb2ebb56fb436
+PYTHON_LIBRARIAN_LOGS_VERSION = v1.1
 PYTHON_LIBRARIAN_LOGS_SITE = $(call github,Outernet-Project,librarian-logs,$(PYTHON_LIBRARIAN_LOGS_VERSION))
 PYTHON_LIBRARIAN_LOGS_LICENSE = GPL
 PYTHON_LIBRARIAN_LOGS_SETUP_TYPE = setuptools
--- python-librarian-ondd/python-librarian-ondd.mk
+++ python-librarian-ondd/python-librarian-ondd.mk
@@ -4,9 +4,14 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ONDD_VERSION = 128ee79b5e1bdab488662502991efea7b5fae758
+PYTHON_LIBRARIAN_ONDD_VERSION = v1.2
 PYTHON_LIBRARIAN_ONDD_SITE = $(call github,Outernet-Project,librarian-ondd,$(PYTHON_LIBRARIAN_ONDD_VERSION))
-PYTHON_LIBRARIAN_ONDD_LICENSE = GPL
+PYTHON_LIBRARIAN_ONDD_LICENSE = GPLv3+
+PYTHON_LIBRARIAN_ONDD_LICENSE_FILES = COPYING
 PYTHON_LIBRARIAN_ONDD_SETUP_TYPE = setuptools
+
+ifeq ($(BR2_PACKAGE_PYTHON_LIBRARIAN_ONDD),y)
+LIBRARIAN_COMPONENTS += librarian_ondd
+endif
 
 $(eval $(python-package))
