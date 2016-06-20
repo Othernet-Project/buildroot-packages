6fe9832d10c0f20c686600958d1eb7bf7e752b7a
Andrean Franc <andrean.franc@gmail.com>
2016-06-20 15:22:41 +0200

    Update librarian and bottle-streamline package versions

--- python-librarian/python-librarian.mk
+++ python-librarian/python-librarian.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_VERSION = 1893b42fc5efb05ccc3822c31fcc49e9f9bbfc2e
+PYTHON_LIBRARIAN_VERSION = d0921a4cfbc885cbd687f3871b111b9c89343db2
 PYTHON_LIBRARIAN_SITE = $(call github,Outernet-Project,librarian,$(PYTHON_LIBRARIAN_VERSION))
 PYTHON_LIBRARIAN_LICENSE = GPLv3+
 PYTHON_LIBRARIAN_LICENSE_FILES = COPYING
