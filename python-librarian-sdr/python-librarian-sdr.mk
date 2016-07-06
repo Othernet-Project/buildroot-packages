5932fd0362c5721679c107aab3efa45baaf62502
Branko Vukelic <branko@outernet.is>
2016-07-06 14:52:43 +0200

    Use includes API and sdr binary path setting
    This patch changes python-librarian-sdr to conform to the new librarian
    includes API and provide its own configuration as an include file. It also
    patches the makefile to use the new BR2_SDR_BINARY_PATH setting.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-librarian-sdr/python-librarian-sdr.mk
+++ python-librarian-sdr/python-librarian-sdr.mk
@@ -4,14 +4,25 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_SDR_VERSION = 4eb537edc92559b5e3b05424c642fad71f53ed45
+PYTHON_LIBRARIAN_SDR_VERSION = d1d6a043714607c8c25841ccaba29c0cf31c08cb
 PYTHON_LIBRARIAN_SDR_SITE = $(call github,Outernet-Project,librarian-SDR,$(PYTHON_LIBRARIAN_SDR_VERSION))
 PYTHON_LIBRARIAN_SDR_LICENSE = GPLv3+
 PYTHON_LIBRARIAN_SDR_LICENSE_FILES = COPYING
 PYTHON_LIBRARIAN_SDR_SETUP_TYPE = setuptools
 
+PYTHON_LIBRARIAN_SDR_SED_CMDS += s|%BINPATH%|$(call qstrip,$(BR2_SDR_BINARY_PATH))|;
+
+define PYTHON_LIBRARIAN_SDR_INSTALL_CONF
+	$(INSTALL) -Dm644 $(call epkgdir,python-librarian-sdr)/sdr.ini \
+		$(TARGET_DIR)/etc/librarian.d/sdr.ini
+	$(SED) '$(PYTHON_LIBRARIAN_SDR_SED_CMDS)' \
+		$(TARGET_DIR)/etc/librarian.d/sdr.ini
+endef
+
 ifeq ($(BR2_PACKAGE_PYTHON_LIBRARIAN_SDR),y)
 LIBRARIAN_COMPONENTS += librarian_sdr
+LIBRARIAN_INCLUDES += /etc/librarian.d/sdr.ini
+TARGET_FINALIZE_HOOKS += PYTHON_LIBRARIAN_SDR_INSTALL_CONF
 endif
 
 $(eval $(python-package))
