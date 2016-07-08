ce62b173a924799aab63b56d8241fd159c52aee1
Branko Vukelic <branko@outernet.is>
2016-07-08 13:10:46 +0200

    Suppress noise in getver output
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- getver.sh
+++ getver.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SCRIPTDIR=$(dirname $0)
+SCRIPTDIR="$(dirname "$0")"
 PKGDIR=$SCRIPTDIR
 
 for d in $PKGDIR/*
 do
-    echo $(basename $d): $(grep VERSION "$d/$(basename $d).mk" | head -n1 | cut -f2 -d=)
+  [ -d "$d" ] || continue
+  echo "$(basename "$d")": $(grep VERSION "$d/$(basename $d).mk" | head -n1 | cut -f2 -d=)
 done
