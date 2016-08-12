5f5bc3cf48f4807075b635c524b4b2dd06c70779
Branko Vukelic <branko@outernet.is>
2016-08-12 20:02:01 +0200

    Update ondd-ipc to development snapshot
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- python-ondd-ipc/python-ondd-ipc.mk
+++ python-ondd-ipc/python-ondd-ipc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_ONDD_IPC_VERSION = v1.0
+PYTHON_ONDD_IPC_VERSION = 9efadd03746f480d663d05d6b9119bf7aa4a3e88
 PYTHON_ONDD_IPC_SITE = $(call github,Outernet-Project,ondd-ipc,$(PYTHON_ONDD_IPC_VERSION))
 PYTHON_ONDD_IPC_LICENSE = GPLv3+
 PYTHON_ONDD_IPC_LICENSE_FILES = COPYING
