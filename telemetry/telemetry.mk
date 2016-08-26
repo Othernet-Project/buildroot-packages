0fae175527cd0e90fdb7bec03400de28345c8e50
Branko Vukelic <branko@outernet.is>
2016-08-26 11:37:45 +0200

    Use epkgdir shortcut instead of BR2_EXTERNAL path
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- telemetry/telemetry.mk
+++ telemetry/telemetry.mk
@@ -9,7 +9,7 @@ TELEMETRY_SITE = $(call github,Outernet-Project,telemetry_client,$(TELEMETRY_VER
 TELEMETRY_SETUP_TYPE = setuptools
 
 define TELEMETRY_INSTALL_INIT_SYSV
-	$(INSTALL) -Dm0755 $(BR2_EXTERNAL)/package/telemetry/S91telemetry \
+	$(INSTALL) -Dm0755 $(call epkgdir,telemetry)/S91telemetry \
 		$(TARGET_DIR)/etc/init.d/S91telemetry
 endef
 
