7896b24d8ed3918f853a40d4d08fa63eb957ddd3
abhishek <abhishek@outernet.is>
2016-10-09 21:44:48 +0530

    add INCRON_CONFDIR param to incron package, so it available to other packages

--- incron/incron.mk
+++ incron/incron.mk
@@ -9,6 +9,7 @@ INCRON_SITE = $(call github,Outernet-Project,incron,$(INCRON_VERSION))
 INCRON_LICENSE = GPLv2
 INCRON_LICENSE_FILES = COPYING
 
+INCRON_SED_CMDS += s|%CONFDIR%|$(call qstrip,$(BR2_INCRON_CONFDIR))|;
 
 define INCRON_BUILD_CMDS
     $(TARGET_MAKE_ENV) \
@@ -18,9 +19,13 @@ define INCRON_BUILD_CMDS
     $(MAKE) -C $(@D) clean incrond
 endef
 
+# note!!! conf dir different from /etc/incrond.conf currently will not work!
+# this is due to the fact that we are actually not yet installing a config that can set the confdir
+# so leave it the default. its only there so that the config var is available in other packages
+
 define INCRON_INSTALL_TARGET_CMDS
 	$(INSTALL) -Dm755 $(@D)/incrond $(TARGET_DIR)/sbin/incrond
-	$(INSTALL) -Dm755 $(TARGET) $(INSTALL_PREFIX)/etc/incron.d/
+	$(INSTALL) -Dm755 $(TARGET) $(INSTALL_PREFIX)$(call qstrip,$(BR2_INCRON_CONFDIR)
 endef
 
 $(eval $(generic-package))
